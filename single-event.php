33acae35e009f00db0deff797b99e464018eb7b1
Diego Granados <diego@codeando.edu.pe>
2022-04-19 13:14:01 -0400

    Pagina de evento singular correcion
    Se agregaron atributos de lightbox. y correciones a los estilos.

--- single-event.php
+++ single-event.php
@@ -12,15 +12,15 @@
         <div class="titulo-general-morado">
             <h2><?php the_title(); ?></h2>
         </div>
-        <a href="<?php echo get_the_post_thumbnail_url(); ?>"><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" class="img-fluid" data-toggle="lightbox" data-gallery="example-gallery"></a>
+        <a href="<?php echo get_the_post_thumbnail_url(); ?>" data-lightbox="galeria"><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" class="img-fluid"></a>
         <?php 
             $images = get_field('galeria'); // (thumbnail, medium, large, full or custom size)
             if( $images ): ?>
                 <ul class="contenedor-galeria">
                     <?php foreach( $images as $image_url ): ?>
                         <li class="imagen-galeria">
-                            <a href="<?php echo $image_url  ?>" data-toggle="lightbox" data-gallery="example-gallery"><img src="<?php echo $image_url ?>" alt=""></a>
-                        </li>
+                            </li>
+                            <a href="<?php echo $image_url  ?>" data-lightbox="galeria" class="enlace-galeria"><img src="<?php echo $image_url ?>" alt="" class="img-galeria"></a>
                     <?php endforeach; ?>
                 </ul>
             <?php endif; ?>
