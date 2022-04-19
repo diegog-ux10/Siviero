8e014e63f0e69a46d6e65f78683b32e2dc9a2630
Diego Granados <diego@codeando.edu.pe>
2022-04-19 13:18:44 -0400

    Estilos para Responsive

--- page-nosse-servicos.php
+++ page-nosse-servicos.php
@@ -1,16 +1,16 @@
 <?php get_template_part( 'template-parts/part', 'banner', ['title' => 'Nossos serviços']); ?>
 
 <section class="contenedor-principal padding">
-    <div class="contenedor-secundario servicio">
+    <div class="contenedor-secundario servicio padding-responsive">
         <div class="columnas">
             <div class="titulo-general-morado"><h2>Armazenagem</h2></div>
             <p class="texto-parrafo">Preocupada com a qualidade dos produtos fez com que a empresa investisse ano a ano no desenvolvimento de soluções para a armazenagem possuindo todos os seus armazéns em condições de prestar um excelente serviço para a manutenção da qualidade do produto, inclusive prestando serviço para o Governo Federal.</p>
         </div>
-        <img src="<?php echo get_theme_file_uri() . "/assets/img/servico01.png" ?>" alt="">
+        <img src="<?php echo get_theme_file_uri() . "/assets/img/servico01.png" ?>" alt="" class="img-fluid">
     </div>
 </section>
 <section class="contenedor-principal padding">
-    <div class="contenedor-secundario servicio">
+    <div class="contenedor-secundario servicio padding-responsive">
         <div class="columnas">
             <div class="titulo-general-morado"><h2>Assitência Técnica</h2></div>
             <p class="texto-parrafo">Disponibilizamos de profissionais capacitados para dar toda a assistência aos seus clientes, informações sobre o mercado, recomendações de cultivo e técnicas de manejo.
