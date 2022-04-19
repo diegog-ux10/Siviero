a34ae1b9e6f4e72c8292c39a4679431c34c5e464
Diego Granados <diego@codeando.edu.pe>
2022-04-19 14:06:11 -0400

    Estilos para Pagina de Contacto

--- page-fale-conosco.php
+++ page-fale-conosco.php
@@ -3,17 +3,17 @@
 <section class="contenedor-principal contacto">
     <div class="contenedor-secundario informacion padding padding-responsive">
         <div class="columnas columna-izquierda">
-            <div class="titulo-general-morado"><h2>Endereço</h2></div>
+            <div class="titulo-general-morado-sin-margen"><h3>Endereço</h3></div>
             <p>Avenida Nossa Senhora da Luz,<br>
             Nº 2005 - Centro<br>
             Clevelândia/PR<br></p>
-            <div class="titulo-general-morado"><h2>Telefone</h2></div>
+            <div class="titulo-general-morado-sin-margen"><h3>Telefone</h3></div>
             <span>(46) 3252-8350</span>
-            <div class="titulo-general-morado"><h2>Email</h2></div>
+            <div class="titulo-general-morado-sin-margen"><h3>Email</h3></div>
                 <a href="mailto:contato@siviero.com.br" style="color: #666666"><p>contato@siviero.com.br</p></a>
         </div>
         <div class="contenedor-formulario columna-derecha">
-        <div class="titulo-general-morado"><h2>Endereço</h2></div>
+        <div class="titulo-general-morado-sin-margen"><h3>Endereço</h3></div>
             <?php echo do_shortcode('[contact-form-7 id="112" title="Formulario de contacto 1"]');?>
         </div>
     </div>
